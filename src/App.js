1f3625e40e906a05d15de60f4a34028590311a2d
hanzala <justhanzala786@gmail.com>
2021-11-19 15:19:19 +0530

    added layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 
 // Pages
 import routes from "./routes";
@@ -10,15 +11,19 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          {routes.map(({ exact, path, component }) => {
-            return (
-              <Route
-                key={path}
-                exact={exact}
-                path={path}
-                component={component}
-              />
-            );
+          {routes.map(({ exact, path, component, auth }) => {
+            if (auth) {
+              return <Layout key={path}></Layout>;
+            } else {
+              return (
+                <Route
+                  key={path}
+                  exact={exact}
+                  path={path}
+                  component={component}
+                />
+              );
+            }
           })}
         </Switch>
       </Router>

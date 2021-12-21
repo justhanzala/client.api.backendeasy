5161e45e7cdb261d4b7e7f059a889372e9563d90
Alqama <alqamaazeem@gmail.com>
2021-12-22 01:43:42 +0530

    Add signup api modal

--- src/pages/PublicApi.js
+++ src/pages/PublicApi.js
@@ -4,32 +4,46 @@ import React from "react";
 import {
   Card,
   CardActionArea,
-  CardMedia,
   CardContent,
   Typography,
+  Box,
+  Modal,
+  TextField,
+  Button,
+  IconButton,
 } from "@mui/material";
+import { Close as CloseIcon } from "@mui/icons-material";
 
 // Component
 import ApiCategories from "../components/ApiCategories";
 
-// Image
-import PublicApiImg from "../assets/img/public-api.png";
-
 const PublicApi = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "100%",
+    height: "100%",
+    bgcolor: "background.paper",
+    boxShadow: 24,
+  };
+
   return (
     <>
       <ApiCategories />
-      <Card sx={{ maxWidth: 500, mr: 3, mb: 4, height: "500px" }}>
+      <Card
+        sx={{ maxWidth: 500, mr: 3, mb: 4, height: "100%" }}
+        onClick={handleOpen}
+      >
         <CardActionArea>
-          <CardMedia
-            component="img"
-            height="250"
-            image={PublicApiImg}
-            alt="Img"
-          />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-              Public APIs
+              Sign Up API
             </Typography>
             <Typography variant="body2" color="text.secondary">
               Lizards are a widespread group of squamate reptiles, with over
@@ -38,6 +52,82 @@ const PublicApi = () => {
           </CardContent>
         </CardActionArea>
       </Card>
+      {/* Modal */}
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={style}>
+          <Box
+            sx={{
+              bgcolor: "primary.main",
+              px: 3,
+              py: 2,
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <IconButton>
+              <CloseIcon className="text-white fs-3" onClick={handleClose} />
+            </IconButton>
+            <Typography variant="h6" color="white" className="ms-3 mb-0">
+              Signup API
+            </Typography>
+          </Box>
+          <div className="container mt-5">
+            <Box component="form">
+              <Box sx={{ width: "100%", display: "flex" }}>
+                <Box sx={{ width: "50%", mr: 2 }}>
+                  <Box sx={{ mb: 4 }}>
+                    <Typography variant="h6" color="black">
+                      Default Fields
+                    </Typography>
+                  </Box>
+                  <Box sx={{ mb: 4 }}>
+                    <TextField
+                      label="API Name"
+                      value="Signup"
+                      fullWidth
+                      disabled
+                      variant="outlined"
+                    />
+                  </Box>
+                  <Box sx={{ mb: 4 }}>
+                    <TextField
+                      label="API Type"
+                      value="POST"
+                      fullWidth
+                      disabled
+                      variant="outlined"
+                    />
+                  </Box>
+                </Box>
+                <Box sx={{ width: "50%" }}>
+                  <Box sx={{ mb: 4 }}>
+                    <Typography variant="h6" color="black">
+                      Payload
+                    </Typography>
+                  </Box>
+                  <Box sx={{ mb: 4 }}>
+                    <TextField label="Name" fullWidth variant="outlined" />
+                  </Box>
+                  <Box sx={{ mb: 4 }}>
+                    <TextField label="Email" fullWidth variant="outlined" />
+                  </Box>
+                </Box>
+              </Box>
+
+              <Box sx={{ width: "100%" }}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  fullWidth
+                  color="primary"
+                >
+                  Submit
+                </Button>
+              </Box>
+            </Box>
+          </div>
+        </Box>
+      </Modal>
     </>
   );
 };
